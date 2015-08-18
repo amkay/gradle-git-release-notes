1d55a01e2ecee20454ef132b3b9c0bdd2a4d209a
Max Käufer <am-kay@web.de>
2015-08-18 18:08:52 +0200

    Add Groovydoc in VersionTagFinder

--- src/main/groovy/com/github/amkay/gradle/git/release/notes/tag/finder/VersionTagFinder.groovy
+++ src/main/groovy/com/github/amkay/gradle/git/release/notes/tag/finder/VersionTagFinder.groovy
@@ -21,12 +21,19 @@ import org.ajoberstar.grgit.Tag
 import org.gradle.api.Project
 
 /**
- * TODO
+ * This strategy for finding the <em>Git tag</em> of the latest release version is used if the version of the project
+ * the plugin was applied on is a normal {@link String}.
  *
  * @author Max Käufer
  */
 class VersionTagFinder implements TagFinder {
 
+    /**
+     * See {@link TagFinder#find(Project, Grgit)}.
+     * @param project
+     * @param grgit
+     * @return
+     */
     @Override
     Tag find(final Project project, final Grgit grgit) {
         def extension = project[ GitReleaseNotesPluginExtension.NAME ] as GitReleaseNotesPluginExtension
