7a12867b0be840412ca66d493a848b56a11f9d51
Max Käufer <am-kay@web.de>
2015-08-18 18:26:06 +0200

    Add package-info for dsl package

---
+++ src/main/groovy/com/github/amkay/gradle/git/release/notes/dsl/package-info.groovy
@@ -0,0 +1,11 @@
+/**
+ * Provides all classes for the well-known <em>keyword-based configuration</em> or <em>DSL</em> that all default
+ * <em>Gradle</em> plugins provide.
+ *
+ * <p>
+ *     The entry point to this configuration is {@link GitReleaseNotesPluginExtension}.
+ * </p>
+ *
+ * @author Max Käufer
+ */
+package com.github.amkay.gradle.git.release.notes.dsl;
