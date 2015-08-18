709b6f547bda27b451023fa47567e74a38aef6e8
Max Käufer <am-kay@web.de>
2015-08-18 18:28:16 +0200

    Add package-info for tag.finder package

---
+++ src/main/groovy/com/github/amkay/gradle/git/release/notes/tag/finder/package-info.groovy
@@ -0,0 +1,11 @@
+/**
+ * Provides the strategies for finding the <em>Git tag</em> of the latest release version of the project to which the
+ * plugin is applied.
+ *
+ * <p>
+ *     The entry point to this package is the interface {@link TagFinder}.
+ * </p>
+ *
+ * @author Max Käufer
+ */
+package com.github.amkay.gradle.git.release.notes.tag.finder;
