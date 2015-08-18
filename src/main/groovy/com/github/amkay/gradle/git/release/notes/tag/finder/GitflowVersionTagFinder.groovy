031a9da6fe3813ee57d45735f5def2d5bf5ec9cf
Max Käufer <am-kay@web.de>
2015-08-18 18:38:23 +0200

    Open link to gradle-gitflow plugin in new window

--- src/main/groovy/com/github/amkay/gradle/git/release/notes/tag/finder/GitflowVersionTagFinder.groovy
+++ src/main/groovy/com/github/amkay/gradle/git/release/notes/tag/finder/GitflowVersionTagFinder.groovy
@@ -23,7 +23,8 @@ import org.gradle.api.Project
 /**
  * This strategy for finding the <em>Git tag</em> of the latest release version is used if the version of the project
  * the plugin was applied on has a <code>normalVersion</code> property. This applies for example if the
- * <a href="https://github.com/amkay/gradle-gitflow">gradle-gitflow</a> plugin is also applied on the project.
+ * <a href="https://github.com/amkay/gradle-gitflow" target="_blank">gradle-gitflow</a> plugin is also applied on the
+ * project.
  * The strategy then searches for a tag matching the <code>normalVersion</code> of the <code>version</code> object of
  * the project, including the configured {@link GitReleaseNotesPluginExtension#versionPrefix}.
  *
